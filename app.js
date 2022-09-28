945b0c019ce526216a8782e81572f7d9d99e77fa
Sebastian Moya <sebastianmoya@192-168-1-110.tpgi.com.au>
2022-09-28 13:41:20 +1000

    erase app.listen

--- app.js
+++ app.js
@@ -9,7 +9,3 @@ app.use(express.static('client'));
 server.listen(PORT, function(){
     console.log('Server running');
 });
-
-app.listen(process.env.PORT || 30000, function() {
-    console.log("Server is running on port 3000")
-});
