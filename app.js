9e2582d6954b09ec546c22df15d3a624468b57c7
Sebastian Moya <sebastianmoya@192-168-1-110.tpgi.com.au>
2022-09-28 14:07:46 +1000

    1

--- app.js
+++ app.js
@@ -11,7 +11,7 @@ app.use(express.static("public"));
 app.use(bodyParser.urlencoded({extended: true}));
 
 app.get("/", function(req, res){
-    res.sendFile(__dirname + "index.html");  
+    res.sendFile(__dirname + "/index.html");  
 });
 
 app.listen(process.env.PORT || 3000, function(){
